0b8ce8f87491f6ac4385a00cf6fa1630befb1ff2
slayercjc <slayercjc@gmail.com>
2018-11-18 08:19:38 +0800

    Site updated: 2018-11-18 08:19:35

---
+++ js/back.top.js
@@ -0,0 +1,15 @@
+$(document).ready(function($){
+var offset = 300,
+    scroll_top_duration = 700,
+    $back_to_top = $('.cd-top');
+$(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+});
+$back_to_top.on('click', function(event){
+    event.preventDefault();
+    $('body,html').animate({
+        scrollTop: 0 ,
+        }, scroll_top_duration
+    );
+});
+});
